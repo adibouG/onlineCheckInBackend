8e048f3437480ca8380fe5db29b46ca7b740d478
adrien gonzalez <adrien@enzosystems.com>
2021-06-17 10:53:37 +0000

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -3,8 +3,8 @@
 set -f 
 string=$DEPLOY_SERVER_URL
 array=(${string//,/ })
-
+    
 for i in "${!array[@]}" ; do
     echo "Deploy on ${array[i]}"
-    ssh ec2-user@${array[i]} "cd /home/ec2-user/gitTest/checkin_backend/checkin_backend && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa $DEPLOY_USER_NAME@$DEPLOY_SERVER_URL" -i ~/.ssh/id_rsa_app ec2-user@$APP_SERVER_URL  "cd Enzo/checkin_api/repo/checkin_backend && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
 done
