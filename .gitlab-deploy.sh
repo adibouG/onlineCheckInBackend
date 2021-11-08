8cdd638ad5f9c07d23d6783f31bee29c3048f98a
adrien gonzalez <adrien@enzosystems.com>
2021-11-08 15:25:45 +0100

    dockerfile update

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,8 +6,8 @@ array=(${string//,/ })
 echo "Deploy"
 for i in "${!array[@]}" ; do
     echo "Deploy on  instance1"
-    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa gitdeploy@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.0.47  "cd Enzo/checkin_api/repo/checkin_backend && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa ec2-user@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.0.47  "cd Enzo/checkin_api/repo/checkin_backend && git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
     echo "Deploy on instance2"
-    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa gitdeploy@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.1.79  "cd Enzo/checkin_api/repo/checkin_backend &&  git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
+    ssh -v -o ProxyCommand="ssh -v -W %h:%p -i ~/.ssh/id_rsa ec2-user@34.244.245.12" -i ~/.ssh/id_rsa_app ec2-user@10.0.1.79  "cd Enzo/checkin_api/repo/checkin_backend &&  git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && npm install"
 
 done
