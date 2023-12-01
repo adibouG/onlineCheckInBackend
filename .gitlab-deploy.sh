a77bd52284575f48740c51d9d98c50cf50228664
iAD <68852816+adibouG@users.noreply.github.com>
2023-12-01 01:35:52 +0100

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -5,8 +5,6 @@ string=$APP_SERVER_URLS
 array=(${string//,/ })
 echo "Deploy"
 #for i in "${!array[@]}" ; do
-    echo "Deploy on  instance1"
-    ssh  -o ProxyCommand="ssh  -W %h:%p -i ~/.ssh/id_deploy $DEPLOY_SERVER_USER@$DEPLOY_SERVER_URL" -i ~/.ssh/id_rsa_app $APP_SERVER_USER@${array[0]}  "cd Enzo/checkin_api/repo/checkin_backend && sudo git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && sudo npm install"
-    echo "Deploy on instance2"
-    ssh  -o ProxyCommand="ssh  -W %h:%p -i ~/.ssh/id_deploy $DEPLOY_SERVER_USER@$DEPLOY_SERVER_URL" -i ~/.ssh/id_rsa_app $APP_SERVER_USER@${array[1]}  "cd Enzo/checkin_api/repo/checkin_backend &&  sudo git pull https://${1}:${2}@gitlab.com/enzo-software-development/checkin_backend.git develop && sudo npm install"
+    echo "Deploy on  instance"
+    ssh  -o ProxyCommand="ssh  -W %h:%p -i ~/.ssh/id_deploy $DEPLOY_SERVER_USER@$DEPLOY_SERVER_URL" -i ~/.ssh/id_rsa_app $APP_SERVER_USER@${i}  "cd ${EC2_APP_PATH} && sudo git pull https://${1}:${2}@${GIT_REPO} ${GIT_BRANCHE} && sudo npm install"
 #done
